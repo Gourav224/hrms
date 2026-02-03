57c6327d292c5aa8708859c347f749eb6c19d13d
Gourav Kumar Dhiman <gkrdhiman@gmail.com>
2026-02-03 18:08:23 +0530

    refactor: move root path redirection from `page.tsx` to `next.config.ts`

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true, // true = 308 permanent redirect, false = 307 temporary
+      },
+    ];
+  },
 };
 
 export default nextConfig;
